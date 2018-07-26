b7557eaf0d791cab3232ffe28ae2674a5b0edb5c
alexanderbronshtein <alexanderbronshtein@10.10.205.112>
2018-07-26 08:28:38 -0500

    Brought back No Args constructor

--- sol-awesome-nomination/src/main/java/com/sol/awesome/nomination/domain/AwesomeEmployee.java
+++ sol-awesome-nomination/src/main/java/com/sol/awesome/nomination/domain/AwesomeEmployee.java
@@ -7,12 +7,14 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id", scope = AwesomeEmployee.class)
 public class AwesomeEmployee {
 	@JsonProperty("id")
