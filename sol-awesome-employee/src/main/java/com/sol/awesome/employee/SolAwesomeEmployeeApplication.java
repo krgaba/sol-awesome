5fc5aeaf20bf167e65e20b54aa101e1cd05bed30
alexanderbronshtein <alexanderbronshtein@192.168.2.10>
2018-07-30 14:15:52 -0500

    Configured implicit logging.

--- sol-awesome-employee/src/main/java/com/sol/awesome/employee/SolAwesomeEmployeeApplication.java
+++ sol-awesome-employee/src/main/java/com/sol/awesome/employee/SolAwesomeEmployeeApplication.java
@@ -1,10 +1,9 @@
 package com.sol.awesome.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
-@EnableAspectJAutoProxy
+
 @SpringBootApplication(scanBasePackages = {"com.sol.awesome"})
 public class SolAwesomeEmployeeApplication {
     public static void main(String[] args) {
--- sol-awesome-employee/src/main/java/com/sol/awesome/employee/repositories/EmployeeRepository.java
+++ sol-awesome-employee/src/main/java/com/sol/awesome/employee/repositories/EmployeeRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.sol.awesome.employee.domain.Employee;
+import com.sol.awesome.util.LogInfo;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -16,12 +18,14 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @Api(tags = "Employee")
+@LogInfo
 @RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 
     @ApiOperation("Find employees by set of ids")
     @ApiResponses({@ApiResponse(code=200, message="Success")})
     List<Employee> findByIdIn(@Param("ids") @ApiParam(value="Employee IDs", required = true) Set<Long> ids);
-
+    
+    
 }
